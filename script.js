f63d25b821133659cfbbbe508d50db2a6c439180
nata753357 <nata753357@mail.ru>
2025-10-22 09:42:06 +0300

    Добавлены изменения ко всем заданиям

--- script.js
+++ script.js
@@ -1,72 +1,74 @@
 // Задание 1
 
-let a = 10;
-alert (a);
-a = 20;
-alert (a);
+//let a = 10;
+//alert (a);
+//a = 20;
+//alert (a);
 
 // Задание 2
 
-let a = 2007;
-alert (a);
+//const dateIPhone = 2007;
+//console. log(dateIPhone);
 
 // Задание 3
 
-// let a = Брэндан Эйх;
-alert (a);
+//const nameCreatorJS = "Брэндан Эйх";
+//console. log(nameCreatorJS);
 
 // Задание 4
-//let a = 10;
-//let a = 2;
+
+//let c = 10;
+//let d = 2;
 
 //let sum = 10 + 2;
-alert (12);
+//alert (12);
 
 //let difference = 10 - 2;
-alert (8);
+//alert (8);
 
 //let product = 10 * 2;
-alert (20);
+//alert (20);
 
 //let quotient = 10 / 2;
-alert (5);
+//alert (5);
 
 // Задание 5
 
 //let result = 2 ** 5;
-alert (32);
+//alert (32);
 
 // Задание 6
 
 //let a = 9;
 //let b = 2;
-alert (1);
+//let result = a % b;
+//alert (1);
 
 // Задание 7
 
-
-let num = 1;
-alert(num += 5);
-alert(num -= 3);
-alert(num *= 7);
-alert(num /= 3);
-alert(num += 1);
-alert(num -= 1);
+//let num = 1;
+//num += 5;
+//num -= 3;
+//num *= 7;
+//num /= 3;
+//num += 1;
+//num -= 1;
+//alert(num);
 
 // Задание 8
 
-//let age = Number (prompt ("Сколько вам лет?"));
-alert (36);
+//let age = prompt ("Сколько вам лет?");
+//alert (age);
 
 // Задание 9
 
 const user = {
-    name: Natalya,
-    age: 36,
-    isAdmin: true,
+//name: "Natalya",
+//age: 36,
+//isAdmin: true,
 }
 
 // Задание 10
 
-const userName = prompt('Как вас зовут?');
-alert(`Привет, ${userName}`);
+//const userName = prompt('Как вас зовут?');
+//alert(`Привет, ${userName}`);
