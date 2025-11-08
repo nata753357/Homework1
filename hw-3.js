2c35c6bc16a3afad4dd59c97d99e2401d5f1185b
nata753357 <nata753357@mail.ru>
2025-11-08 23:40:38 +0300

    Решение задачи 1

---
+++ hw-3.js
@@ -0,0 +1,10 @@
+// Задание 1
+
+let password = 'nata0811';
+let userPass = prompt('Введите пароль');
+
+if (userPass === password) {
+    alert('Пароль введен не верно');
+} alse {
+    alert('Пароль введен неправильно');
+}
