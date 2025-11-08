75a553978916056e8673605d17c61ec6681655fa
nata753357 <nata753357@mail.ru>
2025-11-09 00:04:46 +0300

    Решение задания 5

--- hw-3.js
+++ hw-3.js
@@ -37,3 +37,33 @@ let b = '3';
 
 alert (Number(a) + Number(b));
 
+// Задание 5
+
+let montNumber = 12;
+
+switch (montNumber) {
+    case 1:
+    case 2:
+    case 12:   
+        alert ('зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        alert ('весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        alert ('лето');
+        break;    
+    case 9:
+    case 10:
+    case 11:
+        alert ('осень');
+        break;
+    default:
+        alert ('Неверно');
+        break;     
+}
+
