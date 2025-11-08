f2dc2848e4a16992816366412019833db7c30d17
nata753357 <nata753357@mail.ru>
2025-11-08 23:43:02 +0300

    Удалила ошибку в 1 задании

--- hw-3.js
+++ hw-3.js
@@ -1,10 +1,10 @@
 // Задание 1
 
-let password = 'nata0811';
+let password = 'пароль';
 let userPass = prompt('Введите пароль');
 
 if (userPass === password) {
-    alert('Пароль введен не верно');
-} alse {
+    alert('Пароль введен верно');
+} else {
     alert('Пароль введен неправильно');
 }
