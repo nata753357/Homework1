9c13452e90352730ab607f8fd97dd6d349e95af1
nata753357 <nata753357@mail.ru>
2025-11-09 15:45:13 +0300

    Закоментировала код задач

--- hw-3.js
+++ hw-3.js
@@ -1,108 +1,108 @@
 // Задание 1
 
-let password = 'пароль';
-let userPass = prompt('Введите пароль');
+//let password = 'пароль';
+//let userPass = prompt('Введите пароль');
 
-if (userPass === password) {
-    alert('Пароль введен верно');
-} else {
-    alert('Пароль введен неправильно');
-}
+//if (userPass === password) {
+    //alert('Пароль введен верно');
+//} else {
+   // alert('Пароль введен неправильно');
+//}
 
 // Задание 2
 
-let c = 4
+//let c = 4
 
-if (c >= 0 && c <= 10){
-    console.log ('Верно');
-} else {
-    console.log ('Неверно');
-}
+//if (c >= 0 && c <= 10){
+    //console.log ('Верно');
+//} else {
+    //console.log ('Неверно');
+//}
 
 // Задание 3
 
-let d = 120
-let e = 60
+//let d = 120
+//let e = 60
 
-if (d >= 100 || e >= 100) {
-     console.log ('Верно');
-} else {
-    console.log ('Неверно');
-}
+//if (d >= 100 || e >= 100) {
+     //console.log ('Верно');
+//} else {
+    //console.log ('Неверно');
+//}
 
 // Задание 4
 
-let a = '2';
-let b = '3';
+//let a = '2';
+//let b = '3';
 
-alert (Number(a) + Number(b));
+//alert (Number(a) + Number(b));
 
 // Задание 5
 
-let montNumber = 12;
+//let montNumber = 12;
 
-switch (montNumber) {
-    case 1:
-    case 2:
-    case 12:   
-        alert ('зима');
-        break;
-    case 3:
-    case 4:
-    case 5:
-        alert ('весна');
-        break;
-    case 6:
-    case 7:
-    case 8:
-        alert ('лето');
-        break;    
-    case 9:
-    case 10:
-    case 11:
-        alert ('осень');
-        break;
-    default:
-        alert ('Неверно');
-        break;     
-}
+//switch (montNumber) {
+    //case 1:
+    //case 2:
+    //case 12:   
+        //alert ('зима');
+        //break;
+    //case 3:
+    //case 4:
+    //case 5:
+        //alert ('весна');
+        //break;
+    //case 6:
+    //case 7:
+    //case 8:
+        //alert ('лето');
+        //break;    
+    //case 9:
+    //case 10:
+    //case 11:
+       // alert ('осень');
+        //break;
+    //default:
+       // alert ('Неверно');
+        //break;     
+//}
 
 // Дополнительные задания 
 // Задание 1
 
-let p = (prompt('Пожалуйста, введите любое число'));
-let result = Number (p);
+//let p = (prompt('Пожалуйста, введите любое число'));
+//let result = Number (p);
 
-if (isNaN(result)) {
-    alert ('Не является числом');
-} else {
-    alert ('Является числом');
-}
+//if (isNaN(result)) {
+   // alert ('Не является числом');
+//} else {
+   // alert ('Является числом');
+//}
 
-if (result % 2 === 0) {
-    alert ('Четное число');
-} else {
-    alert ('Нечетное число');
-}
+//if (result % 2 === 0) {
+    //alert ('Четное число');
+//} else {
+   // alert ('Нечетное число');
+//}
 
 // Задание 2
 
-let clientOS = 0
+//let clientOS = 0
 
-if (clientOS === 0) {
-    alert ('Установите версию приложения для iOS по ссылке');
-} else {
-    alert ('Установите версию приложения для Android по ссылке');
-}
+//if (clientOS === 0) {
+    //alert ('Установите версию приложения для iOS по ссылке');
+//} else {
+    //alert ('Установите версию приложения для Android по ссылке');
+//}
 
 // Задание 3
 
-let clientDeviceYear = 2015;
+//let clientDeviceYear = 2015;
 
-if (clientDeviceYear <= 2015) {
-    alert ('Установите облегченную версию приложения для iOS по ссылке')
-} else {
-    alert ('Установите версию приложения для iOS по ссылке')
-}
+//if (clientDeviceYear <= 2015) {
+    //alert ('Установите облегченную версию приложения для iOS по ссылке')
+//} else {
+   // alert ('Установите версию приложения для iOS по ссылке')
+//}
 
 
