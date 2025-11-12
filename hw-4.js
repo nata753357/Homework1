dc43cf2b77c19d81f9c064a406ca19fdb78dc7cc
nata753357 <nata753357@mail.ru>
2025-11-12 13:36:50 +0300

    решение задания 4

--- hw-4.js
+++ hw-4.js
@@ -15,3 +15,15 @@ for (let i = 1; i <= 5; i++) {
 for (let i = 7; i<= 22; i++) {
     console.log (i);
 }
+
+// Задание 4
+
+const obj = {
+    nik:'200',
+    vasya: '300',
+    pety: '400'
+}
+    for (const key in obj) {
+        console.log (`${key} - ${obj[key]}`); 
+        
+    }
