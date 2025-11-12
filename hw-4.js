771383e795229f7c8e6b44451bbb1eaae66bcf03
nata753357 <nata753357@mail.ru>
2025-11-12 13:32:47 +0300

    Решение заданий 1, 2, 3

--- hw-4.js
+++ hw-4.js
@@ -3,3 +3,15 @@
 for (let i = 0; i < 2; i++) {
     console.log ('Привет!');
 }
+
+// Задание 2
+
+for (let i = 1; i <= 5; i++) {
+    console.log (i);
+}
+
+// Задание 3
+
+for (let i = 7; i<= 22; i++) {
+    console.log (i);
+}
