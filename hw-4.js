83b5e07556f3448098cd478def6e4c5c3faec036
nata753357 <nata753357@mail.ru>
2025-11-13 20:47:31 +0300

    Решение доп. задания 4

--- hw-4.js
+++ hw-4.js
@@ -1,84 +1,103 @@
 // Задание 1 
 
- for (let i = 0; i < 2; i++) {
-    console.log ('Привет!');
-}
+ // for (let i = 0; i < 2; i++) {
+ //    console.log ('Привет!');
+// }
 
 // Задание 2
 
-for (let i = 1; i <= 5; i++) {
-    console.log (i);
-}
+ // for (let i = 1; i <= 5; i++) {
+ //     console.log (i);
+ // }
 
 // Задание 3
 
-for (let i = 7; i<= 22; i++) {
-   console.log (i);
-}
+// for (let i = 7; i<= 22; i++) {
+//    console.log (i);
+// }
 
 // Задание 4
 
-const obj = {
-    nik:'200',
-    vasya: '300',
-   pety: '400'
-}
-    for (const key in obj) {
-        console.log (`${key} - ${obj[key]}`); 
-   }
+//const obj = {
+//    nik:'200',
+//    vasya: '300',
+//   pety: '400'
+//}
+//    for (const key in obj) {
+//        console.log (`${key} - ${obj[key]}`); 
+ //  }
 
 // Задание 5
 
-let n = 1000;
-let number = 0;
-while (n >= 50) {
-    n /= 2;    console.log (n);
-    num ++;
-}
-console.log (`Количество циклов: ${num}`);
+// let n = 1000;
+// let number = 0;
+// while (n >= 50) {
+//     n /= 2;    console.log (n);
+//     num ++;
+// }
+// console.log (`Количество циклов: ${num}`);
 
 // Задание 6
 
-let dayFriday = 1;
-let allDay = 31;
+// let dayFriday = 1;
+// let allDay = 31;
 
-for (let i = dayFriday; i <= allDay; i += 7) {
-   console.log (`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);
-}
+// for (let i = dayFriday; i <= allDay; i += 7) {
+//    console.log (`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);
+// }
 
 // Дополнителые задания
 
 // Задание 1
 
-let k = 100;
-let num = 0;
-while (k >= 0) {
-    k -= 7;
-    console.log (k);
-    num ++;
-}
-console.log (`Количество циклов: ${num}`);
+// let k = 100;
+// let num = 0;
+// while (k >= 0) {
+//     k -= 7;
+//     console.log (k);
+//     num ++;
+// }
+// console.log (`Количество циклов: ${num}`);
 
 // Задание 2
 
-const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-for (let i = 0; i < months.length; i ++) {
-    console.log (`${i+1} ${months [i]}`);
-}
+ // const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+ // for (let i = 0; i < months.length; i ++) {
+ //     console.log (`${i+1} ${months [i]}`);
+ // }
 
 // Задание 3
 
- const book = {
+ // const book = {
 
- title: "50 shades of Grey",
- author: "Эрика Леонард Джеймс",
- year: 2011,
- genre: "Драма, триллер, медодрамма, эротика"
- };
+ // title: "50 shades of Grey",
+ // author: "Эрика Леонард Джеймс",
+ // year: 2011,
+ // genre: "Драма, триллер, медодрамма, эротика"
+ // };
 
- console.log ("title:" + book.title);
- console.log ("author:" + book.author);
- console.log ("year:" + book.year);
- console.log ("genre:" + book.genre);
+ // console.log ("title:" + book.title);
+ // console.log ("author:" + book.author);
+ // console.log ("year:" + book.year);
+ // console.log ("genre:" + book.genre);
+
+// Задание 4
+
+const random = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+for ( let min = 0; min <= 10; 
+     i ++) {
+    console.log (`${min+1} ${random [min]}`);
+    console.log (`${min+2} ${random [min]}`);
+    console.log (`${min+3} ${random [min]}`);
+    console.log (`${min+4} ${random [min]}`);
+    console.log (`${min+5} ${random [min]}`);
+    console.log (`${min+6} ${random [min]}`); 
+    console.log (`${min+7} ${random [min]}`);
+    console.log (`${min+8} ${random [min]}`);
+    console.log (`${min+9} ${random [min]}`);
+    console.log (`${min+10} ${random [min]}`);
+
+}
+
 
 
