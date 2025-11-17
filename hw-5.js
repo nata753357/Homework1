4f55b33062fe9f2b71d733c04d5ebb91dd87a67e
nata753357 <nata753357@mail.ru>
2025-11-17 14:12:41 +0300

    Создала новый файл для игры

--- hw-5.js
+++ hw-5.js
@@ -1,1 +1,13 @@
-let whatNumber('Угадай число');
+let whatNumber = prompt('Угадай число');
+
+{
+    if (whatNumber > 0 && whatNumber < 50) {
+        alert('Заданное число больше');
+    } else if (whatNumber > 52 && whatNumber < 100){
+        alert('Заданное число меньше');
+    } else if (whatNumber !== 51) {
+        alert('Угадал');
+    } else {
+        alert('Не понимаю');
+    }
+}
