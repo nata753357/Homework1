156601bbabdc273a28c8882a5b2942ece7916be1
nata753357 <nata753357@mail.ru>
2025-11-25 14:37:42 +0300

    Раскоментировала код задач hw-5.js

--- hw-5.js
+++ hw-5.js
@@ -1,79 +1,81 @@
 // Задание 1
 
-//  function min(a,b) {
-//      if (a < b){
-//          return a;
-//      } else {
-//          return b;
-//      }
-//  }
+  function min(a,b) {
+      if (a < b){
+          return a;
+      } else {
+          return b;
+      }
+  }
 
-// console.log(min(8,4));
-// console.log(min(6,6));
+ console.log(min(8,4));
+ console.log(min(6,6));
 
 // Задание 2
 
-//  function isEven(n) {
-//  if (n % 2 === 0){
-//     return 'Число четное';
-//  } else {
- //    return 'Число нечетное';
-//  }
- // }
+  function isEven(n) {
+  if (n % 2 === 0){
+     return 'Число четное';
+  } else {
+     return 'Число нечетное';
+  }
+  }
 
-// console.log (isEven(2));
-// console.log (isEven(3));
+ console.log (isEven(2));
+ console.log (isEven(3));
 
  // Стрелочная функция
 
-// const isOdd = (n) => (n % 2 === 0) ? 'Число четное' : 'Число нечетное';
+ const isOdd = (n) => (n % 2 === 0) ? 'Число четное' : 'Число нечетное';
 
-// console.log (isOdd(5));
-// console.log (isOdd(4));
+ console.log (isOdd(5));
+ console.log (isOdd(4));
 
 
  // Задание 3
 
- // function square(number) {
- //   console.log(number ** 2);
- // }
+  function square(number) {
+    console.log(number ** 2);
+  }
 
- // const up = (n) => n ** 2;
+   const up = (n) => n ** 2;
+   square (5);
 
- // square (5);
- // console.log (up (5));
+  // Стрелочная функция
+
+    // console.log (up (5));
 
   // Задание 4
 
- //  function age (){
- //    let age = prompt ('Сколько тебе лет');
+   function age () {
+     let age = prompt ('Сколько тебе лет');
 
- //    if (age < 0){
- //        alert ('Возраст не может быть отрицательным');
- //    } else if (age >= 0 && age <= 12) {
- //        alert ('Привет, друг!');
-  //   } else if (age >= 13) {
- //        alert ('Добро пожаловать');
- //    } else {
- //        alert ('Вы ввели неправильное значение');
- //    }
- //  }
+     if (age < 0){
+         alert ('Возраст не может быть отрицательным');
+     } else if (age >= 0 && age <= 12) {
+         alert ('Привет, друг!');
+     } else if (age >= 13) {
+         alert ('Добро пожаловать');
+     } else {
+         alert ('Вы ввели неправильное значение');
+     }
+   }
 
- //    age ();
+     age ();
 
 // Задание 5 
 
-  //  function calc(a,b) {
-  //      console.log (isNaN (a));
-  //      console.log (isNaN (b));
+    function calc(a,b) {
+        console.log (isNaN (a));
+        console.log (isNaN (b));
 
-  //   if (isNaN (a) || isNaN (b)) {
-  //      return 'Одно или оба значения не является числом';
-  //  } else {
-  //      return a + b;
-  //  }
-  //  } 
-   //     console.log (calc (5,2));
+     if (isNaN (a) || isNaN (b)) {
+        return 'Одно или оба значения не является числом';
+    } else {
+        return a + b;
+    }
+    } 
+        console.log (calc (5,2));
 
     // Задание 6
 // 1
@@ -86,36 +88,36 @@
 
 // 2
 
-  // function getNumber() {
-   //    let number = prompt ('Введите число');
-  //      if (isNaN(number)) {
-  //         return 'Переданный параметр не является числом';
-  //     } else {
-  //         return `${number} в кубе равняется ${number ** 3}`;
-  //     }
-  //  }
-  //     console.log(getNumber());
+   function getNumber() {
+       let number = prompt ('Введите число');
+        if (isNaN(number)) {
+           return 'Переданный параметр не является числом';
+       } else {
+           return `${number} в кубе равняется ${number ** 3}`;
+       }
+    }
+       console.log(getNumber());
 
   // Задание 7
 
- // function getArea () {
- //   return Math.PI * this.radius ** 2;
- // }
- //  function getPerimeter () {
-  //  return 2 * Math.PI * this.radius;
- //  }
- //  const circle1 = {
- //   radius: 10,
- //   methodGetArea: getArea,
- //   methodGetPerimeter: getPerimeter,
- //   }
- //   const circle2 = {
-  //  radius: 20,
-  //  methodGetArea: getArea,
-  //  methodGetPerimeter: getPerimeter,
-  //  } 
-  //  console.log(circle1.methodGetArea());
-  //  console.log(circle2.methodGetArea());
+  function getArea () {
+    return Math.PI * this.radius ** 2;
+  }
+   function getPerimeter () {
+    return 2 * Math.PI * this.radius;
+   }
+   const circle1 = {
+    radius: 10,
+    methodGetArea: getArea,
+    methodGetPerimeter: getPerimeter,
+    }
+    const circle2 = {
+    radius: 20,
+    methodGetArea: getArea,
+    methodGetPerimeter: getPerimeter,
+    } 
+    console.log(circle1.methodGetArea());
+    console.log(circle2.methodGetArea());
 
   
 
