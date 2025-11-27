31306cdb2d9f6715507881f9f6a674c615448236
nata753357 <nata753357@mail.ru>
2025-11-27 14:21:40 +0300

    Решение заданий 1, 2, 3

---
+++ hw-6.js
@@ -0,0 +1,25 @@
+// Задание 1
+
+const arr = [1, 5, 4, 10, 0, 3]
+for (let i = 0; i < arr.length; i ++) {
+    console.log (arr[1]);
+    if (arr [i] === 10) {
+        break;
+    }
+}
+
+// Задание 2
+
+const array = [1, 5, 4, 10, 0, 3]
+for (let i = 0; i < array.length; i ++) {
+    if (array [i] === 4) {
+        console.log (i);
+        break;
+    }
+}
+
+// Задание 3
+
+const Array = [1, 3, 5, 10, 20];
+console.log (Array.join(' '));
+
