e1da7a2bd566ab0fcd5a6c82fd19086a454b2f08
nata753357 <nata753357@mail.ru>
2026-01-10 14:12:25 +0300

    Создание игры Викторина

--- games.js
+++ games.js
@@ -77,6 +77,50 @@
     let reverseText = text.split('').reverse().join('');
         alert (reverseText);
  }
+
+ function startGame5() {
+    const quiz = [
+        {
+            question: "Какой цвет неба?",
+            options: ["1. Красный", "2. Синий", "3. Зеленый"],
+            correctAnswer: 2,
+        },
+        {
+            question: "Сколько дней в неделе?",
+            options: ["1. Шесть", "2. Семь", "3. Восемь"],
+            correctAnswer: 2
+        },
+        {
+            question: "Сколько у человека пальцев на одной руке?",
+            options: ["1. Четыре", "2. Пять", "3. Шесть"],
+            correctAnswer: 2
+        },
+        {
+             question: "Сколько дней в году?",
+            options: ["1. Пятьсот", "2. Триста шестьдесят пять", "3. Четыреста"],
+            correctAnswer: 2  
+        }
+    ];
+
+    let num = 0
+    for ( i = 0; i < quiz.length; i++) {
+        let quizQuest = Number(prompt(`Выбери правильный ответ? ${quiz[i].question} ${quiz[i].options}`));
+            if (quizQuest === quiz[i].correctAnswer) {
+             num++;
+                alert("Вы ответили верно");
+            }else{
+                alert ("Не верно");
+            }
+    }
+
+         alert(i);
+}
+       
+
+
+   
+
+
     
 
 
