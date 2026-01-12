0c6040f1d48d6f1a9f49752f3a61a663877d6720
nata753357 <nata753357@mail.ru>
2026-01-12 16:07:33 +0300

    Решение задания 2

--- hw-8.js
+++ hw-8.js
@@ -9,3 +9,33 @@ const people = [
 
 console.log(people.sort((a,b) => a.age - b.age));
 
+// Задание 2
+
+function filter (array, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction (array [i])) {
+            result.push (array [i]);
+        }
+    }
+        return result;
+}
+
+function isPositive (num) {
+    return num > 0;
+}
+
+function isMale (person) {
+    return person.gender === 'male';
+}
+
+console.log (filter ([3, -4, 1, 9], isPositive));
+
+const people1 = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people1, isMale));
